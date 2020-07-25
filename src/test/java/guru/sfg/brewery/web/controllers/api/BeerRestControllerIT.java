d243c168a907a5b67e38c1de13cb5f88b0d49a6e
Jonathan T. Cohen <jcohen66@optonline.net>
2020-07-25 10:26:33 -0400

    matchers protecting endpoints

---
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -0,0 +1,30 @@
+package guru.sfg.brewery.web.controllers.api;
+
+
+import guru.sfg.brewery.web.controllers.BaseIT;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class BeerRestControllerIT extends BaseIT {
+    @Test
+    void findBeers() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findBeerById() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/f4a15ef2-ce1c-11ea-87d0-0242ac130003"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findBeerByUpc() throws Exception {
+        mockMvc.perform(get("/api/v1/beerUpc/0631234200836"))
+                .andExpect(status().isOk());
+    }
+}
